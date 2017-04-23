383ef14cf5670ac3b95730b07572d72e040ddbde
jssmk <jussmaki@fastmail.fm>
2017-04-23 09:36:47 +0300

    URL regex for months

--- project/creditor/urls.py
+++ project/creditor/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     url(r'^transaction_table/(?P<year>[0-9]{4})/$', permission_required('is_staff')(views.TransactionYearView.as_view()), name="table_y"),
     url(r'^transaction_table/(?P<year>[0-9]{4})/tag=(?P<tag>[0-9]+)$', permission_required('is_staff')(views.TransactionYearView.as_view()), name="table_y"),
     
-    url(r'^transaction_table/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/$', permission_required('is_staff')(views.TransactionMonthView.as_view()), name="table_m"),
-    url(r'^transaction_table/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/tag=(?P<tag>[0-9]+)$', permission_required('is_staff')(views.TransactionMonthView.as_view()), name="table_m"),
+    url(r'^transaction_table/(?P<year>[0-9]{4})/(?P<month>(1[0-2]|0[1-9]))/$', permission_required('is_staff')(views.TransactionMonthView.as_view()), name="table_m"),
+    url(r'^transaction_table/(?P<year>[0-9]{4})/(?P<month>(1[0-2]|0[1-9]))/tag=(?P<tag>[0-9]+)$', permission_required('is_staff')(views.TransactionMonthView.as_view()), name="table_m"),
 ]
