7ae25b06f5dbfc83263273a3ee4c2e37747a1a9b
jssmk <jussmaki@fastmail.fm>
2017-09-04 21:29:59 +0300

    Update tests using new urlspace

--- project/creditor/tests/test_transaction_table.py
+++ project/creditor/tests/test_transaction_table.py
@@ -3,18 +3,18 @@ import pytest
 
 # Should always redirect
 def test_views_nonadmin(client):
-    assert client.get('/creditor/transaction_table/').status_code == 302
-    assert client.get('/creditor/transaction_table/2000/').status_code == 302
-    assert client.get('/creditor/transaction_table/2000/01/').status_code == 302
-    assert client.get('/creditor/transaction_table/2000/tag=0').status_code == 302
-    assert client.get('/creditor/transaction_table/2000/01/tag=0').status_code == 302
+    assert client.get('/admin/transaction_table/').status_code == 302
+    assert client.get('/admin/transaction_table/2000/').status_code == 302
+    assert client.get('/admin/transaction_table/2000/01/').status_code == 302
+    assert client.get('/admin/transaction_table/2000/tag=0').status_code == 302
+    assert client.get('/admin/transaction_table/2000/01/tag=0').status_code == 302
 
 
 # Only admins should be able to see the table
 def test_views_admin(admin_client):
-    assert admin_client.get('/creditor/transaction_table/').status_code == 200
-    assert admin_client.get('/creditor/transaction_table/2000/').status_code == 200
-    assert admin_client.get('/creditor/transaction_table/2000/01/').status_code == 200
-    assert admin_client.get('/creditor/transaction_table/2000/tag=0').status_code == 200
-    assert admin_client.get('/creditor/transaction_table/2000/01/tag=0').status_code == 200
+    assert admin_client.get('/admin/transaction_table/').status_code == 200
+    assert admin_client.get('/admin/transaction_table/2000/').status_code == 200
+    assert admin_client.get('/admin/transaction_table/2000/01/').status_code == 200
+    assert admin_client.get('/admin/transaction_table/2000/tag=0').status_code == 200
+    assert admin_client.get('/admin/transaction_table/2000/01/tag=0').status_code == 200
 
